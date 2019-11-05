5e02c55158273c25b5830c5732d2a01c084bad6e
Eric Rowan <erow80@gmail.com>
2019-11-04 20:16:34 -0800

    Remove gatsby-plugin-sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,7 +15,6 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-offline`,
-    `gatsby-plugin-sitemap`,
     {
       resolve: `gatsby-plugin-mdx`,
       options: {
