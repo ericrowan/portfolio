93aad8b788c04b85e577af33d9cc323bd8c80a7d
Eric Rowan <erow80@gmail.com>
2019-11-04 20:05:55 -0800

    React Helmet

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,7 +16,6 @@ module.exports = {
     `gatsby-plugin-sharp`,
     `gatsby-plugin-offline`,
     `gatsby-plugin-sitemap`,
-    `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-plugin-mdx`,
       options: {
