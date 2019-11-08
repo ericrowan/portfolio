cfb84dda3847a224fc8bd6bbd760199ef5c8f4f7
Eric Rowan <erow80@gmail.com>
2019-11-08 11:13:39 -0800

    Favicon: favor png for now

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -2,7 +2,7 @@ import React from "react"
 import { Helmet } from "react-helmet"
 import PropTypes from "prop-types"
 import { StaticQuery, graphql } from "gatsby"
-import favicon from "../images/favicon.svg"
+import favicon from "../images/favicon.png"
 
 const SEO = ({ title, description, image, pathname, article }) => (
   <StaticQuery
